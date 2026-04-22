bb44352800854700b563cd8543ba95effdb11a82
mboulemnazil8683-design <m.boulemnazil8683@uca.ac.ma>
2026-04-22 20:59:17 +0100

    test2

---
+++ script.js
@@ -0,0 +1,9 @@
+function Togglemenu(){
+    const d =document.getElementById("dropdown")
+    if (d.style.display==="block"){
+        d.style.display="none"
+    }
+    else{
+        d.style.display==="block"
+    }
+}
